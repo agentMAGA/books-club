109bd9d2750c5d0c362aeeb04b428b81fc5c91d4
big-black-pixel <hasamovmohmad@gmail.com>
2025-09-22 11:28:03 +0300

    Удолил не неужное

--- src/App.jsx
+++ src/App.jsx
@@ -38,44 +38,6 @@ function App() {
 
         </section>
 
-{/* ////////////////////////////////////////////////////////////////////////////// просто разделение, при жеалании можно удалить*/}
-
-        <div className="visualBackdrop">
-
-          <h4 className="titleEvents">Мероприятия</h4>
-
-          <nav>
-            <button aria-label="Предыдущий">‹</button>
-          </nav>
-
-          <div>
-
-            <article>
-              <p>
-                Morem ipsum dolor sit amet,
-                consectetur adipiscing elit.
-                Etiam eu turpis molestie, dictum est a, mattis tellus.
-                Sed dignissim, metus nec...
-              </p>
-            </article>
-
-            <article className="timeRangeDate">
-              <p>24.08.2025 13:00 -<br />24.08.2025 14:00</p>
-            </article>
-
-            <div>
-              <span><img src="img\usersIcons.svg" alt="usersIcons" /></span>
-              <span>14</span>
-            </div>
-
-          </div>
-
-          <nav>
-            <button aria-label="Следующий">›</button>
-          </nav>
-
-        </div>
-
       </main>
     </>
   )
