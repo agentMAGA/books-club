e8ad0ec7d5dfcff6de446315361f8e0dbf97cd9d
Магомед <magomed@MacBook-Air-Magomed.local>
2025-09-25 11:01:55 +0300

    Доделал настройки

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,17 @@
 import Newspaper from "./pages/newspaper";
-import Header from "./components/Header";
 import "./scss/app.scss";
 import { Route, Routes } from "react-router";
-import Footer from "./components/Footer";
 import LiveSessions from "./pages/LiveSessions";
+import Settings from "./pages/Settings";
 
 function App() {
   return (
     <>
-      <Header />
       <Routes>
         <Route path="/newspaper" element={<Newspaper />} />
         <Route path="/livesessions" element={<LiveSessions />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
-      <Footer />
-      {/* <main>
-
-        <section className="innerSpacing">
-
-          <h1>Books Club</h1>
-          <p>Космос идей. Вселенная книг.</p>
-          <button>Присоединиться</button>
-
-        </section>
-
-        <section className="innerSpacing">
-
-          <h2>О нас </h2>
-          <p>
-            Arête — это постоянное стремление к совершенству и полной реализации своего потенциала.
-            Мы начали с книг, ведь именно они зажигают в нас первые звезды любознательности.
-            Но наш путь не заканчивается на этом — он только начинается.
-          </p>
-
-          <p>Наш клуб — это созвездие искателей.  </p>
-
-          <p>Мы — навигаторы в безграничном космосе искусства, дизайна, кино, литературы и многого другого.</p>
-
-          <p>
-            Здесь мы помогаем друг другу прокладывать путь к своим звездам,
-            делимся взглядами для расширения кругозора и вдохновением для души.
-          </p>
-
-          <p>Мы оставляем границы позади, чтобы достичь своей Arête.</p>
-
-        </section>
-
-      </main> */}
     </>
   );
 }
