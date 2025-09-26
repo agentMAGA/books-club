943ca9fe66214b8dfc73580b76f5063a010cf357
Магомед <magomed@MacBook-Air-Magomed.local>
2025-09-26 15:43:11 +0300

    Сделал бургер меню

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../scss/components/header.module.scss";
+import BurgerMenu from "./BurgerMenu";
 
 const Header = () => {
-    return (
-      <header>
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  return (
+    <>
+      <header className={styles.header}>
         <img className={styles.headerIcon} src="img/logo.svg" alt="icon" />
-        <h2>Arête</h2>
-        <img className={styles.burgerMenu} src="img/burger-menu.svg" alt="menu"/>
+        <h2 className={styles.title}>Arête</h2>
+        <img
+          className={styles.burgerMenu}
+          src="img/burger-menu.svg"
+          alt="menu"
+          onClick={toggleMenu}
+        />
       </header>
-    );
+
+      {menuOpen && <BurgerMenu closeMenu={closeMenu} />}
+    </>
+  );
 };
 
 export default Header;
