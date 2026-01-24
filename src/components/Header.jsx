e429bee5c45b7dfc007ae610a90e5ca1cf7b1e3e
big-black-pixel <hasamovmohmad@gmail.com>
2026-01-24 10:04:14 +0300

    Востановил свою ветку

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { NavLink , Link} from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import styles from "../scss/components/header.module.scss";
 import BurgerMenu from "./BurgerMenu";
 import { useTheme } from "../store/useTheme";
@@ -7,8 +7,8 @@ import { useTheme } from "../store/useTheme";
 const Header = () => {
   const { theme, toggleTheme } = useTheme();
   const [menuOpen, setMenuOpen] = useState(false);
-  const openMenu = () => {setMenuOpen(!menuOpen)};
-  const closeMenu = () => {setMenuOpen(false)};
+  const openMenu = () => { setMenuOpen(!menuOpen) };
+  const closeMenu = () => { setMenuOpen(false) };
 
   return (
     <>
@@ -21,21 +21,21 @@ const Header = () => {
         <img className={styles.burgerMenu} src="img/burger-menu.svg" alt="menu" onClick={openMenu} />
         {menuOpen && <BurgerMenu closeMenu={closeMenu} />}
 
-        <nav className={theme === 'black' ? styles.desktopMenu : `${styles.desktopMenu} ${styles.desktopMenuColor}`}>
-        <NavLink to="/" className={({ isActive }) => (isActive ? styles.active : "")}>ГЛАВНАЯ</NavLink>
-        <NavLink to="/happenings" className={({ isActive }) => (isActive ? styles.active : "")}>МЕРОПРИЯТИЯ</NavLink>
-        <NavLink to="/newspaper" className={({ isActive }) => (isActive ? styles.active : "")}>ГАЗЕТА</NavLink>
-        <NavLink to="/rating" className={({ isActive }) => (isActive ? styles.active : "")}>РЕЙТИНГ</NavLink>
-      </nav>
-      <div 
+        <nav className={styles.desktopMenu}>
+          <NavLink to="/" className={({ isActive }) => (isActive ? styles.active : "")}>ГЛАВНАЯ</NavLink>
+          <NavLink to="/happenings" className={({ isActive }) => (isActive ? styles.active : "")}>МЕРОПРИЯТИЯ</NavLink>
+          <NavLink to="/newspaper" className={({ isActive }) => (isActive ? styles.active : "")}>ГАЗЕТА</NavLink>
+          <NavLink to="/rating" className={({ isActive }) => (isActive ? styles.active : "")}>РЕЙТИНГ</NavLink>
+        </nav>
 
-      className={ theme === 'black' ? styles.bleackOn : styles.bleackOff }
-      onClick={() => toggleTheme()}
+        <div
+          className={theme === 'black' ? styles.bleackOn : styles.bleackOff}
+          onClick={() => toggleTheme()}>
+          <div className={styles.polzunoc}></div></div>
 
-      ><div className={styles.polzunoc}></div></div>
-      <Link to={'login'}>
-        <button className={ theme === 'black' ? styles.loginButton : `${styles.loginButton} ${styles.loginButtonColor}`}>ВОЙТИ</button>
-      </Link>
+        <Link to={'login'}>
+          <button className={theme === 'black' ? styles.loginButton : `${styles.loginButton} ${styles.loginButtonColor}`}>ВОЙТИ</button>
+        </Link>
       </header>
 
     </>
